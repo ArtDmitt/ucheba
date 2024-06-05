6f9843e291ec66d4b3479d83b19981f06651680a
ArtDmit <ArtemDm2005@gmail.com>
2024-06-05 20:30:18 +0300

    first commit

---
+++ libs/algorithms/algorithms.c
@@ -0,0 +1,3 @@
+//
+// Created by Artem on 28.05.2024.
+//
---
+++ libs/algorithms/algorithms.h
@@ -0,0 +1,8 @@
+//
+// Created by Artem on 28.05.2024.
+//
+
+#ifndef LABA13_ALGORITHMS_H
+#define LABA13_ALGORITHMS_H
+
+#endif //LABA13_ALGORITHMS_H
